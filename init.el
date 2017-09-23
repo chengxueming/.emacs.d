3bd4f9f71ec210d725d4eb306c4da841be377685
chengxueming <=>
2017-09-23 12:57:51 +0800

    first commit

---
+++ init.el
@@ -0,0 +1,30 @@
+(custom-set-variables
+;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 	  ;; jdee-mode
+ ;; '(add-to-list 'load-path "~/.emacs.d/jdee-2.4.1/lisp")  
+ ;; '(load "jde")
+ ;; php-mode
+  ;; (autoload 'php-mode "php-mode" "PHP editing mode." t)
+ ;; '(setq auto-mode-alist (cons '("\\.php$" . php-mode) auto-mode-alist))
+      ;;(load-theme 'leuven)
+'(set-default-font "-outline-Axure Handwriting-bold-normal-normal-sans-21-*-*-*-p-*-iso8859-1")
+      '(define-key global-map [C-return] 'set-mark-command)
+    '(send-mail-function (quote smtpmail-send-it))
+  '(smtp mail-smtp-server "smtp.qq.com")
+  '(smtp mail-smtp-service 25)
+  '(setq user-full-name "前人种树")
+ '(setq user-mail-address "2694149918@qq.com")
+ '(setq-default tab-width 4)
+ '(setq-default indent-tabs-mode)
+ 
+    (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+  )
+)
+;;(put 'upcase-region 'disabled nil)
