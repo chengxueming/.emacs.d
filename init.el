675ef7a7e65897861ff07fe7d2ce4bad1738a950
CHENGXUEMING [程学明] <2694149918@qq.com>
2017-09-26 20:35:13 +0800

    add melpa to package list and install neotree for emacs

--- init.el
+++ init.el
@@ -2,10 +2,14 @@
               '("melpa" . "http://melpa.org/packages/"))
 (set-default-font "-outline-Axure Handwriting-bold-normal-normal-sans-21-*-*-*-p-*-iso8859-1")
 (define-key global-map [C-return] 'set-mark-command)
+;;neotree begin
 (add-to-list 'load-path "~/.emacs.d/elpa/neotree-20170522.758")
+;;Every time when the neotree window is opened, let it find current file and jump to node.
 (setq neo-smart-open t)
+;;When running ‘projectile-switch-project’ (C-c p p), ‘neotree’ will change root automatically.
  (setq projectile-switch-project-action 'neotree-projectile-action)
- (global-set-key [f8] 'neotree-toggle)
+(global-set-key [f8] 'neotree-toggle)
+;;neotree end
   ;;(send-mail-function (quote smtpmail-send-it))
  ;; (smtp mail-smtp-server "smtp.qq.com")
  ;; (smtp mail-smtp-service 25)
